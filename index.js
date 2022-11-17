5d54a844576bf3b96ed092e864906fd7e3fd267b
Asif Nawaz <assu.nawaz007@gmail.com>
2022-11-17 20:02:51 +0500

    added js function
    added submitfunction and then reterive data and declared the data which are to me reterived , then return it with arr , then declare readdata to reading data from local storage . and pass the declare variable of reterive data in it

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// CRUD .. Create,Read,Update and Delete
+
+// Golbal Variables
+function Submit() {
+    var dataEntered = retrieveData(); // retrieve data data after on click submit 
+    var readData = readingDataFromLocalStorage(dataEntered);
+}
+    function retrieveData() {
+        var name1 = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var number =  document.getElementById("number").value;
+    
+        var arr = [name1,email,number];
+        return arr;
+    }
