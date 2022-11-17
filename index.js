4358df156c47562b44e32a0808c6ab66ca9a1c16
Asif Nawaz <assu.nawaz007@gmail.com>
2022-11-17 22:30:50 +0500

    added
     READ => updated data in local storage, getting value to the table

--- index.js
+++ index.js
@@ -5,20 +5,35 @@
 function Submit() {
     var dataEntered = retrieveData(); // retrieve data data after on click submit 
     var readData = readingDataFromLocalStorage(dataEntered);
+    console.log(readData);
 }
 
 // CREATE
 // retriving data from form
     function retrieveData() {
         var name1 = document.getElementById("name").value;
-        var email = document.getElementById("email").value;
-        var number =  document.getElementById("number").value;
+        var email1 = document.getElementById("email").value;
+        var number1 =  document.getElementById("number").value;
     
-        var arr = [name1,email,number];
+        var arr = [name1,email1,number1];
         return arr;
     }
 
+
+    // READ
     // data in local storage
     function readingDataFromLocalStorage (dataEntered){
+        // Storing data in local storage
+        var na = localStorage.setItem("name", dataEntered[0]);
+        var em = localStorage.setItem("email", dataEntered[1]);
+        var nu = localStorage.setItem("number", dataEntered[2]);
+    
+        // getting values from local to table
+        var na1 = localStorage.getItem("name", na);
+        var em1 = localStorage.getItem("email", em);
+        var nu1 = localStorage.getItem("number", nu);
 
-    }
+        var arr = [na1, em1, nu1];
+        return arr;
+
+    } 
