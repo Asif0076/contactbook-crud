1088cc178bfa3521d3d53026018497f5661b4520
Asif Nawaz <assu.nawaz007@gmail.com>
2022-11-17 20:22:03 +0500

    added comment for perticular line

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function Submit() {
     var dataEntered = retrieveData(); // retrieve data data after on click submit 
     var readData = readingDataFromLocalStorage(dataEntered);
 }
+
+// CREATE
+// retriving data from form
     function retrieveData() {
         var name1 = document.getElementById("name").value;
         var email = document.getElementById("email").value;
@@ -13,4 +16,9 @@ function Submit() {
     
         var arr = [name1,email,number];
         return arr;
+    }
+
+    // data in local storage
+    function readingDataFromLocalStorage (dataEntered){
+
     }
